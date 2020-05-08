76f40fa56790d2f724dde1b704968d1ee49784b9
Victor Dyomin <dyomin_vic@ukr.net>
2020-05-08 18:56:30 +0300

    Add font

--- src/components/weather-daily-component/weather-daily-component.jsx
+++ src/components/weather-daily-component/weather-daily-component.jsx
@@ -14,15 +14,22 @@ class DailyWeather extends Component {
   render() {
     let forecast = this.props.forecast;
     let date = new Date(forecast.dt * 1000);
+    let dateClass = "weather-dialy__label--date";
     let dateFormatted = date.toLocaleDateString(
       "uk-ua", 
       { month: "2-digit", day: "2-digit" });
+
+    let dayFormatted = date.toLocaleDateString("en-us", { weekday: "short" }).toLowerCase();
+    if(dayFormatted == "sat" || dayFormatted == "sun"){
+      dateClass += " weather-dialy__label--date--offday";
+    }
+    
     let minTemperatureFormatted = Math.round(forecast.temp.min);
     let maxTemperatureFormatted = Math.round(forecast.temp.max);
     return (
       <div>
         <div className="weather-dialy">
-          <p className="weather-dialy__label--date">{dateFormatted}</p>
+          <p className={dateClass}>{dateFormatted}</p>
           <p className="weather-dialy__label--temperature">
             {minTemperatureFormatted} <span className="celsius">C°</span>
             –
